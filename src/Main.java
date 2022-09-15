18062a80fb06d8886768f105fcb9f132c2a53a47
Anatoliy Bondarenko <zagedan_28@mail.ru>
2022-09-15 16:29:29 +0300

    DZ 2-1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 100;
+        int spent = 20;
+        int bonusMiles = ticketPrice / spent;
+        System.out.println(bonusMiles);
+    }
+}
